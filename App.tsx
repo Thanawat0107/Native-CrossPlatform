5b5d2dbb0dfe9265bc3d60f7faa3d633074ebeac
KENG!sBackward <qwwasszxx2345@gmail.com>
2024-12-28 19:01:25 +0700

    My-ProjectTEST

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>MY-PROJECTTT!!!</Text>
+      <Text>My-ProjectTEST</Text>
       <StatusBar style="auto" />
     </View>
   );
