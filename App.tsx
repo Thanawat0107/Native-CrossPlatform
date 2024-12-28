6c2ae09719082fdba548e5ae8cc01e99435e8993
thanawat01 <qwwasszxx2345@gmail.com>
2024-12-28 18:51:03 +0700

    MY-PROJECTTT!!!

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>MY-PROJECT!!</Text>
+      <Text>MY-PROJECTTT!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
