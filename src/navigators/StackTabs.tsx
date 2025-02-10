aea8b6e01bda57f5322600336d2298009070d231
KENG!sBackward <qwwasszxx2345@gmail.com>
2025-02-10 21:48:47 +0700

    ลบส่วนที่ไม่ได้ใช้

--- src/navigators/StackTabs.tsx
+++ src/navigators/StackTabs.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { CartScreen, HomeScreen, NewRivalsScreen, SettingScreen } from '../screen';
+import { CartScreen, NewRivalsScreen } from '../screen';
 import { ProductDetails } from '../components';
 import { CategorySetting, ProductSetting, ProductUpsert } from '../components/settting';
 import RootTabs from './RootTabs';
@@ -49,60 +49,6 @@ const StackTabs = () => {
       />
     </Stack.Navigator>
   )
-}
-
-const HomeStack = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{ headerShown: false, }}
-      />
-      <Stack.Screen
-        name="ProductDetails"
-        component={ProductDetails}
-        options={{ headerShown: false, }}
-      />
-      <Stack.Screen
-        name="ProductList"
-        component={NewRivalsScreen}
-        options={{ headerShown: false, }}
-      />
-      <Stack.Screen
-        name="Cart"
-        component={CartScreen}
-        options={{ headerShown: false, }}
-      />
-    </Stack.Navigator>
-  );
-}
-
-const SettingStack = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Setting"
-        component={SettingScreen}
-        options={{ headerShown: false, }}
-      />
-      <Stack.Screen
-        name="ProductSetting"
-        component={ProductSetting}
-        options={{ headerShown: false, }}
-      />
-      <Stack.Screen
-        name="ProductUpsert"
-        component={ProductUpsert}
-        options={{ headerShown: false, }}
-      />
-      <Stack.Screen
-        name="CategorySetting"
-        component={CategorySetting}
-        options={{ headerShown: false, }}
-      />
-    </Stack.Navigator>
-  );
 }
 
 export {StackTabs }
